2db0c8e93b0308dc6fc48f5198836f82809faea6
Alex Le <thanhquang.le@grabtaxi.com>
2019-10-12 04:17:33 +0800

    Add UI elements

---
+++ src/components/UI/Button/Button.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Button from './Button';
+
+describe('Button', () => {
+  test('renders without crashing', () => {
+    const component = renderer.create(<Button />);
+    const json = component.toJSON();
+
+    expect(json).toMatchSnapshot();
+  });
+
+  test('renders without loading', () => {
+    const component = renderer.create(<Button loading />);
+    const json = component.toJSON();
+
+    expect(json).toMatchSnapshot();
+  });
+});
