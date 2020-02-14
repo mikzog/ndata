547b1859a3b7cf0524baf3a20c9c8b5031bae9da
mikzog <magicmikevv@gmail.com>
2020-02-14 18:22:56 +0800

    Upgrade React dependencies and fixed heap issue

--- src/components/properties/s3-section/s3-general-form.tsx
+++ src/components/properties/s3-section/s3-general-form.tsx
@@ -1,47 +1,65 @@
-import _get from 'lodash/get';
-import React, { PureComponent } from 'react';
-import { Form, Input, Row, Col } from 'antd';
-import { FormComponentProps } from 'antd/lib/form/Form';
+import React from 'react';
+import { S3_GENERAL_INIT_DATA } from './constants';
 
-interface Props extends FormComponentProps {
+interface Props {
   data?: any;
   onChange?: (values: any) => void;
 }
 
-export class S3GeneralForm extends PureComponent<Props> {
-  static defaultProps: { data: never[] };
+const S3GeneralForm: React.FC<Props> = ({ data, onChange }) => {
+  return (
+    <>
+      {/*<Row>
+          <Col span={12}>
+            <Form layout="vertical">
+              <Form.Item label="Name">
+                {getFieldDecorator('name', {
+                  initialValue: _get(data, 'name'),
+                  rules: [{ required: true }],
+                })(<Input />)}
+              </Form.Item>
+              <Form.Item label="Description">
+                {getFieldDecorator('description', {
+                  initialValue: _get(data, 'description'),
+                })(<Input.TextArea />)}
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>*/}
+      <div className="nd-input-group disable">
+        <label className="nd-label">Name <span
+          style={{ color: 'red' }}>*</span></label>
+        <input id="modal-pillar" type="text" className="nd-input"
+               placeholder="Dummy text here" value=""/>
+      </div>
 
-  render() {
-    const { data, form } = this.props;
-    const { getFieldDecorator } = form;
+      <div className="nd-input-group disable">
+        <label className="nd-label">Description</label>
+        <textarea className="nd-input"
+                  placeholder="Dummy text here"
+                  value="">Dummy text here</textarea>
+      </div>
+    </>
+  );
+};
 
-    return (
-      <Row>
-        <Col span={12}>
-          <Form layout="vertical">
-            <Form.Item label="Name">
-              {getFieldDecorator('name', {
-                initialValue: _get(data, 'name'),
-                rules: [{ required: true }],
-              })(<Input />)}
-            </Form.Item>
-            <Form.Item label="Description">
-              {getFieldDecorator('description', {
-                initialValue: _get(data, 'description'),
-              })(<Input.TextArea />)}
-            </Form.Item>
-          </Form>
-        </Col>
-      </Row>
-    );
-  }
-}
+// export class S3GeneralForm extends PureComponent<Props> {
+//   static defaultProps: { data: never[] };
+//
+//   render() {
+//     // const { data, form } = this.props;
+//     // const { getFieldDecorator } = form;
+//
+//
+//   }
+// }
 
-export default Form.create<Props>({
-  name: 's3-general',
-  onValuesChange({ onChange }, values) {
-    if (onChange) {
-      onChange(values);
-    }
-  },
-})(S3GeneralForm);
+export default S3GeneralForm;
+// export default Form.create<Props>({
+//   name: 's3-general',
+//   onValuesChange({ onChange }, values) {
+//     if (onChange) {
+//       onChange(values);
+//     }
+//   },
+// })(S3GeneralForm);
